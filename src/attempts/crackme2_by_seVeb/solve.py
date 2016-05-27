bb34532cda8e07820767539bbf36025cd11422aa
Patrick <patrick.brand2@gmail.com>
2016-05-27 16:24:07 +0200

    updated constraints. index 14 to 17 are now even numbers (actually only the sum of those should be even, but this was easier to implement.

--- src/attempts/crackme2_by_seVeb/solve.py
+++ src/attempts/crackme2_by_seVeb/solve.py
@@ -3,11 +3,9 @@
 import angr
 
 start_addr = 0x400CE7 # Start of Main function
-#find_addr = 0x400D77 # 2nd compare function requires d as first input
-#find_addr = 0x400b68 # Pepper get past second compare
-find_addr = 0x400E36
+find_addr = 0x400E36 # Random address near the end of the program
 avoid_addr = 0x400D8D # Start of hades function, when serial is invalid
-input_addr = 0x602100
+input_addr = 0x602100 # Address where user inputted license key is stored
 input_length = 21#0x15 # Derived from 0x400D66 where rax is string input_length
 
 
@@ -15,41 +13,39 @@ def extract_memory(state):
     """Convience method that returns the flag input memory."""
     return state.se.any_str(state.memory.load(input_addr, input_length))
 
-def char(state, n):
-    """Returns a symbolic BitVector and contrains it to printable chars for a given state."""
-    vec = state.se.BVS('c{}'.format(n), 8, explicit_name=True)
-    return vec, state.se.And(vec >= ord(' '), ord('~'))
-
 def main():
     project = angr.Project('crackme02_64bit')
     
-    print('input constraints')
+    print "Initialize solver"
+    special_conditions = []
+    print "-Set state of program to address: " + str(hex(start_addr))
     state = project.factory.blank_state(addr=start_addr)
+    print "-Add constraints: "
     for i in range(input_length):
-        c, cond = char(state, i)
-        # the first command line argument is copied to INPUT_ADDR in memory
-        # so we store the BitVectors for angr to manipulate
-        state.memory.store(input_addr + i, c)
-        print cond
-        state.add_constraints(cond)
+        symb_vec = state.se.BVS("c"+str(i), 8, explicit_name=True) # Create a symbolic value for each character (8bits)
+        constraint = state.se.And(symb_vec >= ord(' '), symb_vec <= ord('~')) # Each input character is between whitespace and ~
+        if i >= 14 and i <= 17 :
+            constraint = state.se.And(symb_vec % 2 == 0, state.se.And(symb_vec >= ord(' '), symb_vec <= ord('~')))
+        state.memory.store(input_addr + i, symb_vec) # Put the symbolic value at the location of the character of the 'input string' 
+        state.add_constraints(constraint) # Add constraint to the symbolic execution engine
+        print "--"+str(constraint)
         
-    print('path and explorer')
+    print "Initialize explorer"
     path = project.factory.path(state)
+    print "-Path: "+str(path)
+    print "-Find addr: "+str(hex(find_addr))
+    print "-Avoid addr: "+str(hex(avoid_addr))
     explorer = project.surveyors.Explorer(start=path, find=(find_addr,), avoid=(avoid_addr,))
     
-    print('run explorer')
+    print "Run explorer"
     explorer.run()
     
-    flag = extract_memory(explorer._f.state) # ex._f is equiv. to ex.found[0]
-    print('found flag: {}'.format(flag))
-    #for f in explorer.found:
-    #    flag = flag = extract_memory(f.state)
-    #    print('found flag: {}'.format(flag))
+    print "Extract result from memory"
+    flag = extract_memory(explorer.found[0].state)
+    print "license key: "+str(flag)
 
     return flag
 
 
 if __name__ == '__main__':
     a = main()
-    print [ord(c) for c in a]
-    #print a
